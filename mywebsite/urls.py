a3003b8ae0d52340df7160afd0806dcae3cfd715
Francisco <francisco.salema.g@gmail.com>
2020-05-25 19:39:20 +0200

    Added blog and resume templates

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("home.urls")),
+    path('resume/', include("resume.urls")),
+    path('blog/', include("blog.urls")),
     path('trips/', include("album.urls")),
     path('admin/', admin.site.urls),
 ]
